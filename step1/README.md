77b1e212454ffdbcc8b6bbcb579b352b40321eff
cyprien piron <cyprien.piron@gmail.com>
2022-06-18 12:33:45 +0200

    updated step0, added step1

---
+++ step1/README.md
@@ -0,0 +1,6 @@
+# Step 1  
+Next up, we're gonna try to get your Epitech's credits and GPA.  
+This is a bit trickier as you need to use your autologin link. No more 
+information, you'll look it up by yourself..
+  
+https://intra.epitech.eu/admin/autolog
--- step1/step1.py
+++ step1/step1.py
@@ -2,13 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 
 yourstring = "the string you found"
-url = "http://www.krosmoz.com/en/almanax"
+url = "epitech"
 
 # Make a GET request to fetch the raw HTML content
+response = requests.get(url)
 
 # Parse the html content
 
-# Finding the "fleft" class in the "p" tag (Almanax's daily offering)
-
 # Print the string you found
-print("String found : {}".format(yourstring))
+print("String found : {}".format(response.text))
